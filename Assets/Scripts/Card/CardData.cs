a17e0bda11491ff45acf3ee095a77ae6ef8bb935
Culkin <culkin.nikluc@gmail.com>
2025-11-22 00:01:08 +0530

    Imported TextMeshPro essentials for use of fonts and sprite assets for testing and ease of use, Implemented a scene with for testing alignments and Created a small scriptable object script to create card samples
    Signed-off-by: Culkin <culkin.nikluc@gmail.com>

---
+++ Assets/Scripts/Card/CardData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MemoryGame/Card")]
+public class CardData : ScriptableObject
+{
+    public string cardName;
+    public Sprite cardSprite;
+    public Color cardColor;
+    public AudioClip flipSound;
+}
